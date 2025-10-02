626293735001414a83e89ca38111d1642b473e28
ssacko <#s19LU89>
2025-10-02 11:14:25 +0000

    Mise à jour pour la prise en comptes des attributs "type" et "typeLocation".

--- social_protection/custom_filters.py
+++ social_protection/custom_filters.py
@@ -96,7 +96,9 @@ class BenefitPlanCustomFilterWizard(CustomFilterWizardInterface):
                         tuple_with_definition = tuple_type(
                             field=key,
                             filter=self.FILTERS_BASED_ON_FIELD_TYPE[value['type']],
-                            type=value['type']
+                            type=value['type'],
+                            referential=value['referential'] if 'referential' in value else None,
+                            typeLocation=value['typeLocation'] if 'typeLocation' in value else None
                         )
                         tuples_with_definitions.append(tuple_with_definition)
                         existing_keys.add(key)
